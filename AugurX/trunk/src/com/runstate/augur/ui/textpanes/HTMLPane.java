6a42a31f4d9a0790baefaf84857e95d894802a5d
adam.a.baker <adam.a.baker@86503216-1026-0410-aae3-8f851685636a>
2007-02-26 22:54:07 +0000

    Issue: 1 Add a translation table to HTML pane to fix up the characters that aren't correctly decoded by the HTMLEditorKit. Fix the project source encoding as CP1252 so the translation table works. (The About Panel already used CP1252 and was broken when compiled as UTF-8).

--- AugurX/trunk/src/com/runstate/augur/ui/textpanes/HTMLPane.java
+++ AugurX/trunk/src/com/runstate/augur/ui/textpanes/HTMLPane.java
@@ -1,5 +1,5 @@
 /**
- * JMutedTextPane.java
+ * HTMLPane.java
  *
  * @author Created by Omnicore CodeGuide
  */
@@ -7,6 +7,7 @@
 package com.runstate.augur.ui.textpanes;
 
 import com.runstate.augur.controller.Controller;
+import java.lang.String;
 import javax.swing.*;
 
 import com.runstate.augur.AugurX;
@@ -38,6 +39,51 @@ public class HTMLPane extends JTextPane {
 			setFont(defaultfont);
 		}
 		
+                /* Map the characters that are not handled by Java's HTMLEditorKit
+                 * current sources can only generate characters that are part of
+                 * CP-1252, the HTMLEditorKit only maps those that are in 8859-1
+                 * so we need to correct the few that are in CP1252 but not 8859-1
+                 * here.
+                 *
+                 * Don't touch this code unless your editor is set to CP 1252
+                 *
+                 * References for the HTML codes and the content of CP1252 are
+                 * http://www.w3.org/2000/07/8378/xhtml/entities/entities.xml
+                 * http://www.microsoft.com/typography/unicode/1252.htm
+                 */
+                text=text.replaceAll("\\&euro;","€");       // 80
+                // 81 - Undefined
+                text=text.replaceAll("\\&sbquo;","‚");      // 82
+                text=text.replaceAll("\\&fnof;","ƒ");       // 83
+                text=text.replaceAll("\\&bdquo;","„");      // 84
+                text=text.replaceAll("\\&hellip;","…");     // 85
+                text=text.replaceAll("\\&dagger;","†");     // 86
+                text=text.replaceAll("\\&Dagger;","‡");     // 87
+                text=text.replaceAll("\\&circ;","ˆ");       // 88
+                text=text.replaceAll("\\&permil;","‰");     // 89
+                text=text.replaceAll("\\&Scaron;","Š");     // 8A
+                text=text.replaceAll("\\&lsaquo;","‹");     // 8B
+                text=text.replaceAll("\\&OElig;","Œ");      // 8C
+                // 8D - Undefined
+                // 8E - No HTML escape
+                // 8F - Undefined
+                // 90 - Undefined
+                text=text.replaceAll("\\&lsquo;","‘");      // 91
+                text=text.replaceAll("\\&rsquo;","’");      // 92
+                text=text.replaceAll("\\&ldquo;","“");      // 93
+                text=text.replaceAll("\\&rdquo;","”");      // 94
+                text=text.replaceAll("\\&bull;","•");       // 95
+                text=text.replaceAll("\\&ndash;","–");      // 96
+                text=text.replaceAll("\\&mdash;","—");      // 97
+                text=text.replaceAll("\\&tilde;","˜");      // 98
+                text=text.replaceAll("\\&trade;","™");      // 99
+                text=text.replaceAll("\\&scaron;","š");     // 9A
+                text=text.replaceAll("\\&rsaquo;","›");     // 9B
+                text=text.replaceAll("\\&oelig;","œ");      // 9C
+                // 9D - Undefined
+                // 9E - No HTML escape
+                text=text.replaceAll("\\&Yuml;","Ÿ");      // 9F
+     
 		super.setText(text);
 	}
 	
